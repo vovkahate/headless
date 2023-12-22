96f55dfce542ed4f09118b575e0a5d7181bf1bb4
vovka <lesnikov.vladimir@gmail.com>
2023-12-22 23:57:51 +0300

    1

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { cashReducer } from './cashReducer';
 import { customerReducer } from './customerReducer';
+import { thunk } from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -9,4 +10,10 @@ const rootReducer = combineReducers({
     customers: customerReducer,
 });
 
-export const store = createStore(rootReducer, composeEnhancers());
+export const store = createStore(
+    rootReducer,
+    compose(
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(),
+        applyMiddleware(thunk)
+    )
+);
